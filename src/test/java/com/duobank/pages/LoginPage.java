43138d8408712d7b894a67a584ab391956fe4d26
MJ77777 <106645719+MJ77777@users.noreply.github.com>
2022-07-20 12:33:09 -0400

    Adding LogIn Elements

--- src/test/java/com/duobank/pages/LoginPage.java
+++ src/test/java/com/duobank/pages/LoginPage.java
@@ -1,4 +1,20 @@
 package com.duobank.pages;
 
+import com.duobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "exampleInputEmail1")
+public WebElement EnterEmailAddress;
+
+    @FindBy(id = "exampleInputPassword1")
+    public WebElement EnterPassword;
+
+    @FindBy(name = "login")
+    public WebElement ClickOnLogin;
 }
